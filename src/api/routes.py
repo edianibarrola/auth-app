1e7b8194df7d3c054806eb129be2c6b245a95bab
edianibarrola <edianibarrola@gmail.com>
2021-08-25 16:57:36 +0000

    m

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,35 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/register', methods=['POST'])
+def handle_register():
+    user = request.get_json() 
+
+    resp =  {
+        "msg":"here is the user",
+        "user": user
+    }
+
+    return jsonify(resp), 200
+
+@api.route('/login', methods=['POST'])
+def handle_login():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
+
+@api.route('/userinfo', methods=['GET'])
+def get_user_info():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
